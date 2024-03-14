de7b3cbebae3dc0caebd7ee7e8106891ae46641c
L7aur <gradlaur@yahoo.com>
2024-03-14 08:55:45 +0200

    clean up production version

--- src/main/java/org/example/processing/Patterns.java
+++ src/main/java/org/example/processing/Patterns.java
@@ -7,38 +7,25 @@ public class Patterns {
     public static ArrayList<Pattern> patterns2;
     static {
         patterns2 = new ArrayList<>();
-        patterns2.add(Pattern.compile("[-|+]\\b[0-9]+\\*X\\^[0-9]+"));    // +-number * X ^ number
-        patterns2.add(Pattern.compile("[0-9]+\\*X\\^[0-9]+"));               // number * X ^ number
+        patterns2.add(Pattern.compile("[-|+]\\b[0-9]+\\*X\\^[0-9]+"));  // +-number * X ^ number
+        patterns2.add(Pattern.compile("[0-9]+\\*X\\^[0-9]+"));          // number * X ^ number
 
-        patterns2.add(Pattern.compile("[-|+]\\b[0-9]+X\\^[0-9]+"));       // +-number X ^ number
-        patterns2.add(Pattern.compile("[0-9]+X\\^[0-9]+"));       // number X ^ number
+        patterns2.add(Pattern.compile("[-|+]\\b[0-9]+X\\^[0-9]+"));     // +-number X ^ number
+        patterns2.add(Pattern.compile("[0-9]+X\\^[0-9]+"));             // number X ^ number
 
-        patterns2.add(Pattern.compile("[-|+]\\b[0-9]+\\*X"));               // +-number * X
-        patterns2.add(Pattern.compile("[0-9]+\\*X"));               // number * X
+        patterns2.add(Pattern.compile("[-|+]\\b[0-9]+\\*X"));           // +-number * X
+        patterns2.add(Pattern.compile("[0-9]+\\*X"));                   // number * X
 
-        patterns2.add(Pattern.compile("[-|+]\\b[0-9]+X"));                  // +-number X
-        patterns2.add(Pattern.compile("[0-9]+X"));                  // +-number X
+        patterns2.add(Pattern.compile("[-|+]\\b[0-9]+X"));              // +-number X
+        patterns2.add(Pattern.compile("[0-9]+X"));                      // +-number X
 
-        patterns2.add(Pattern.compile("[-|+]\\bX\\^[0-9]+"));             // +- X ^ number
-        patterns2.add(Pattern.compile("X\\^[0-9]+"));             //  X ^ number
+        patterns2.add(Pattern.compile("[-|+]\\bX\\^[0-9]+"));           // +- X ^ number
+        patterns2.add(Pattern.compile("X\\^[0-9]+"));                   //  X ^ number
 
-        patterns2.add(Pattern.compile("[-|+]\\bX"));                        // +- X
-        patterns2.add(Pattern.compile("X"));                        //  X
+        patterns2.add(Pattern.compile("[-|+]\\bX"));                    // +- X
+        patterns2.add(Pattern.compile("X"));                            //  X
 
-        patterns2.add(Pattern.compile("[-|+]\\b[0-9]+"));                     // +-number
-        patterns2.add(Pattern.compile("[0-9]+"));                     // number
+        patterns2.add(Pattern.compile("[-|+]\\b[0-9]+"));               // +-number
+        patterns2.add(Pattern.compile("[0-9]+"));                       // number
     }
-
-//    public static ArrayList<Pattern> patterns;
-//    static {
-//        patterns = new ArrayList<>();
-//        patterns.add(Pattern.compile("[-]?[+]?\\b[0-9]+[*][X][\\^][0-9]+"));    // +-number * X ^ number
-//        patterns.add(Pattern.compile("[-]?[+]?\\b[0-9]+[X][\\^][0-9]+"));       // +-number X ^ number
-//        patterns.add(Pattern.compile("[-]?[+]?\\b[0-9]+[*][X]"));               // +-number * X
-//        patterns.add(Pattern.compile("[-]?[+]?\\b[0-9]+[X]"));                  // +-number X
-//        patterns.add(Pattern.compile("[-]?[+]?\\b[X][\\^][0-9]+"));             // +- X ^ number
-//        patterns.add(Pattern.compile("[-]?[+]?\\b[X]"));                        // +- X
-//        patterns.add(Pattern.compile("[-]?[+]?\\b[0-9]+"));                     // +-number
-//        //check expressions X200 maybe double passing through the string and checking the end result or switching pattern order
-//    }
 }
